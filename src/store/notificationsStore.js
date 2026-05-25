ff3b4e11fcf34209788a9687a23af8db9ab09e33
Iris-Ghislaine <ganzairis03@gmail.com>
2026-05-25 16:11:44 +0200

    feat: add notifications store

---
+++ src/store/notificationsStore.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+import { mockNotifications } from '../data/mockNotificationsData'
+
+export const useNotificationsStore = create((set) => ({
+  items: mockNotifications.map((n) => ({ ...n })),
+  markAllRead: () =>
+    set((s) => ({
+      items: s.items.map((n) => ({ ...n, read: true })),
+    })),
+  markRead: (id) =>
+    set((s) => ({
+      items: s.items.map((n) => (n.id === id ? { ...n, read: true } : n)),
+    })),
+}))
